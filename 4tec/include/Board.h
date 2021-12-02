255fa85a7d49eef136e506dd2b7e3f3214c2b345
ben-millar <c00236772@itcarlow.ie>
2021-12-02 01:31:16 +0000

    Added buffer row and col to board to prevent wrapping of connect-4 between rows/layers

--- 4tec/include/Board.h
+++ 4tec/include/Board.h
@@ -7,16 +7,37 @@
 class Board
 {
 public:
+	/// <summary>
+	/// Tries to make a move on the board for the current player, provided it's legal
+	/// </summary>
+	/// <param name="layer">Layer to place on</param>
+	/// <param name="row">Row to place on</param>
+	/// <param name="col">Col to place on</param>
+	/// <returns>True if move was successful, false otherwise</returns>
 	bool makeMove(uint8_t layer, uint8_t row, uint8_t col);
-private:
 
-	uint8_t _layers{ 4 }, _rows{ 4 }, _cols{ 4 };
+private:
+	// We have a playable area of 4x4x4
+	uint8_t _validLayers{ 4 }, _validRows{ 4 }, _validCols{ 4 };
+
+	// But we add a buffer row/col to each layer to prevent 4-in-a-row from wrapping across boundaries
+	uint8_t _layers{ _validLayers }, _rows{ _validRows+1u }, _cols{ _validCols+1u };
 
 	// Keep track of the current player's tokens (XOR with board to swap players)
-	std::bitset<4*4*4> _currentPlayerTokens;
+	std::bitset<4*5*5> _currentPlayerTokens;
 
 	// Keep track of all tokens on board, regardless of colour
-	std::bitset<4*4*4> _board;
+	std::bitset<4*5*5> _board;
+
+	/* Mask off our buffer rows / cols to ensure they're always 0, e.g.
+	* 1 1 1 1 0
+	* 1 1 1 1 0
+	* 1 1 1 1 0
+	* 1 1 1 1 0
+	* 0 0 0 0 0 */
+	std::bitset<4 * 5 * 5> _bufferMask{
+		"1111011110111101111000000111101111011110111100000011110111101111011110000001111011110111101111000000"
+	};
 };
 
 #endif BOARD_H
--- 4tec/src/Board.cpp
+++ 4tec/src/Board.cpp
@@ -3,7 +3,7 @@
 
 bool Board::makeMove(uint8_t layer, uint8_t row, uint8_t col)
 {
-	if (layer > _layers || row > _rows || col > _cols)
+	if (layer > _validLayers || row > _validRows || col > _validCols)
 		return false;
 
 	// E.g., 1/1/1 = 1(16) + 1(4) + 1 = 21
@@ -20,6 +20,9 @@ bool Board::makeMove(uint8_t layer, uint8_t row, uint8_t col)
 
 		// Add last player's token
 		_board.set(index, true);
+
+		// Ensure buffer rows/cols are 0 (is this necessary?)
+		_board &= _bufferMask;
 
 		return true;
 	}
